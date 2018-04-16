26bde9e6113a5ab0ee06900a63b05b00235ad3e5
Julio <juliomartelosso@gmail.com>
2018-04-16 18:02:37 +0200

    Fetch example

---
+++ app.js
@@ -0,0 +1,54 @@
+document.querySelector('#button1').addEventListener('click', getText);
+document.querySelector('#button2').addEventListener('click', getJSON);
+document.querySelector('#button3').addEventListener('click', getAPI);
+
+let output = document.querySelector('#output');
+
+function getText(){
+    fetch('test.txt')
+        .then(function(res){
+            return res.text();            
+        })
+        .then(function(data){
+            console.log(data);
+            output.innerHTML = data;
+        })
+        .catch(function(err){
+            output.innerHTML = err;
+        })
+}
+
+function getJSON(){
+    fetch('test.json')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            let out = '';
+            data.forEach(function(post){
+                out += `<li>${post.body}</li>`
+            })
+            output.innerHTML = out;
+        })
+        .catch(function(err){
+            output.innerHTML = err;           
+        })
+}
+
+function getAPI(){
+    fetch('https://api.github.com/users')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            let out = '';
+            let i = 1;
+            data.forEach(function(user){
+                out += `<li>${i++} - ${user.login}</li>`
+            })
+            output.innerHTML = out;
+        })
+        .catch(function(err){
+            output.innerHTML = err;
+        })
+}
